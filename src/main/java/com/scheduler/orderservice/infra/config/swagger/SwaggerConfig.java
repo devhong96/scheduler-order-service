870ff7c7d651c15c14b1939a880ffbff2467fb93
devhong96 <dev.hong96@gmail.com>
2025-08-04 15:28:13 +0900

    modify : swagger

--- src/main/java/com/scheduler/orderservice/infra/config/swagger/SwaggerConfig.java
+++ src/main/java/com/scheduler/orderservice/infra/config/swagger/SwaggerConfig.java
@@ -1,18 +1,22 @@
 package com.scheduler.orderservice.infra.config.swagger;
 
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(title = "Order Service Api")
-)
 
 @Configuration
-@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Order Service Api").version("1.0"))
+                .addServersItem(new Server().url("/scheduler-order-service").description("API Gateway URL"))
+                .addServersItem(new Server().url("/").description("Local DEV Server"));
+    }
 
 
 }
